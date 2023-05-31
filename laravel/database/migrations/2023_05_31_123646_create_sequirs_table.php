3efa748e379bed4f3f3b8cd324d8f67577a982e9
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-05-31 10:39:24 -0300

    adição da migracion

--- laravel/database/migrations/2023_05_31_123646_create_sequirs_table.php
+++ laravel/database/migrations/2023_05_31_123646_create_sequirs_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 Schema::enableForeignKeyConstraints();
+
 return new class extends Migration
 {
     /**
@@ -11,22 +12,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sequirs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('foto');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('sequido_fk')->constrained(
+                table: 'users'
+            );
+            $table->foreignId('sequidor_fk')->constrained(
+                table: 'users'
+            );
             $table->timestamps();
         });
     }
+
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sequirs');
     }
 };

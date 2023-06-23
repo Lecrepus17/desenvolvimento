d04fe2cc0a4b1e7c4e4263252c0cdb911743a52f
Cybelle <cybellegbem@gmail.com>
2023-06-23 08:21:59 -0300

    .

--- laravel/resources/views/bootstrap/index.blade.php
+++ laravel/resources/views/bootstrap/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>FOI App Landing Page</title>
+    <title>AmiGo</title>
     <link rel="stylesheet" href="assets/vendors/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -15,7 +15,7 @@
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light foi-navbar">
                 <a class="navbar-brand" href="index.html">
-                    <img src="assets/images/prof_de_bio.png" alt="FOI">
+                    <img src="assets/images/prof_de_bio.png" alt="Quokka">
                 </a>
                 <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -23,10 +23,10 @@
                 <div class="collapse navbar-collapse" id="collapsibleNavId">
                     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                         <li class="nav-item active">
-                            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="index">Página Inicial <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.html">About</a>
+                            <a class="nav-link" href="about.html">Sobre</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="features.html">Features</a>
@@ -36,21 +36,21 @@
                             <div class="dropdown-menu" aria-labelledby="pagesMenu">
                                 <a class="dropdown-item" href="blog.html">Blog</a>
                                 <a class="dropdown-item" href="login.html">Login</a>
-                                <a class="dropdown-item" href="register.html">Register</a>
-                                <a class="dropdown-item" href="faq.html">FAQ</a>
+                                <a class="dropdown-item" href="register.html">Cadastro</a>
+                                <a class="dropdown-item" href="faq.html">Perguntas Frequentes</a>
                                 <a class="dropdown-item" href="404.html">404</a>
-                                <a class="dropdown-item" href="careers.html">Careers</a>
-                                <a class="dropdown-item" href="blog-single.html">Single blog</a>
-                                <a class="dropdown-item" href="privacy-policy.html">Privacy policy</a>
+                                <a class="dropdown-item" href="careers.html">Carreiras</a>
+                                <a class="dropdown-item" href="blog-single.html">Blog Teste</a>
+                                <a class="dropdown-item" href="privacy-policy.html">Política de Privacidade</a>
                             </div>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="contact.html">contact</a>
+                            <a class="nav-link" href="contact.html">Contato</a>
                         </li>
                     </ul>
                     <ul class="navbar-nav mt-2 mt-lg-0">
                         <li class="nav-item mr-2 mb-3 mb-lg-0">
-                            <a class="btn btn-secondary" href="register.html">Sign up</a>
+                            <a class="btn btn-secondary" href="register.html">Cadastro</a>
                         </li>
                         <li class="nav-item">
                             <a class="btn btn-secondary" href="login.html">Login</a>
@@ -61,8 +61,8 @@
             <div class="header-content">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1>Great app that makes your life awesome</h1>
-                        <p class="text-dark">He has led a remarkable campaign, defying the traditional mainstream parties courtesy of his En Marche! movement.</p>
+                        <h1>Junte-se ao AmiGo!</h1>
+                        <p class="text-dark">Torne-se um Amigo e descubra vários outros Amigos na nossa rede social maravilhosa!</p>
                         <button class="btn btn-primary mb-4">Get Started</button>
                         <div class="my-2">
                             <p class="header-app-download-title">GET OUR MOBILE APP</p>

1bdf78bfa6714872e929bdbd610ff60060cd969f
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-08-10 20:49:56 -0300

    ajeitando comentarios

--- laravel/resources/views/bootstrap/postagens.blade.php
+++ laravel/resources/views/bootstrap/postagens.blade.php
@@ -193,7 +193,7 @@
 													<div class="post-comt-box">
 														<form method="post" action="{{ route("createComent")}}">
                                                             <input type="hidden" name="user_fk" value="{{ $userAuth->id }}">
-                                                            <input type="hidden" name="post_fk" value="{{ $userAuth->id }}">
+                                                            <input type="hidden" name="post_fk" value="{{ $post->id }}">
 
 															<textarea tyepe="text" name="texto" placeholder="faça seu comentário"></textarea>
 															<li>

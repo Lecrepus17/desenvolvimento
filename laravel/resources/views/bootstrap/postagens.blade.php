f6840dc364d012bc08c254a101a5cc4efc5af8c0
Cybelle Bem <cybellegbem@gmail.com>
2023-08-09 22:11:27 -0300

    FeedSeguindo Pronto

--- laravel/resources/views/bootstrap/postagens.blade.php
+++ laravel/resources/views/bootstrap/postagens.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>AmiGo</title>
+	<title>AmiGo - Feed</title>
     <link rel="icon" href="assets/images/favicon.png" type="image/png" sizes="16x16">
 
     <link rel="stylesheet" href="css/main.min.css">
